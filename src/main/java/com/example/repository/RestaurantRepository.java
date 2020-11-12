cd2992eadb3904d6759f3243d843988b2f712ac1
Rafael Anderson <rp_9966@hotmail.com>
2020-11-11 22:59:55 -0500

    Microservice-Management

---
+++ src/main/java/com/example/repository/RestaurantRepository.java
@@ -0,0 +1,11 @@
+package com.example.repository;
+
+import com.example.entity.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant,Long> {
+
+    Restaurant findRestaurantsById(Long id);
+}
